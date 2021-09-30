38a75d9a87f7aa4bbc02bcf614a5775503f537ce
Sami Byers <samtaylor.byers@gmail.com>
2021-09-30 18:47:11 -0500

    almost there

--- assets/script.js
+++ assets/script.js
@@ -5,11 +5,10 @@ $("#currentDay").text(currentDay);
 //this sets the current hour
 var currentHour = moment().format("k"); //k sets to military time
 console.log(currentHour);
-//there is already a class for each row
-var row = $(".row")
-
-// grabing each text area with class description box looping through them and console logging "this" which refers ti each item that is being looped through
-$.each($(".description"), function() {
+//var for the texArea
+var textArea = $(".description")
+// grabing each text area with class description box, looping through them, and console logging "this.id" which refers to each item that is being looped through
+$.each(textArea, function() {
     console.log(this.id)
     if (parseInt(this.id) < currentHour) {
         //make the description area .past
@@ -23,6 +22,18 @@ $.each($(".description"), function() {
     }
 })
 
+//add variable for the save button
+var saveBtn = $(".saveBtn")
+//add an event listener for the save button
+saveBtn.on("click", function() {
+    console.log("hi")
+//when the save button is clicked, the text is stored to local storage
+var storedText = $(this).siblings(".description").val()
+localStorage.setItem("storedText", storedText)
+})
 
+//get item from local storage when page is refreshed
 
+var storedText = localStorage.getItem("storedText")
 
+storedText.val(localstorage.getItem("storedText"))
